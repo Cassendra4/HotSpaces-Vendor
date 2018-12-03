d4ea258ca88b3628ccc81e35458a17b5de7c244f
Cassendra4 <hasangi44@hotmail.com>
2018-12-03 15:18:45 +0530

    committing changes

--- HotSpaces-Vendor/readQR.js
+++ HotSpaces-Vendor/readQR.js
@@ -1,42 +1,10 @@
 let AWS = require('aws-sdk');
 let dynamoDBService = require('./dynamoDBService');
-let authService = require('./authService');
 
 exports.handler = function (event, context, callback) {
 
- let userUUID = event.queryStringParameters.uuid;
-    let userName = event.queryStringParameters.user;
-    
-     authService.validateUser(userUUID, userName, function (response) {
-        if (response.error) {
-            callback(null, {
-                "isBase64Encoded": true,
-                "statusCode": 502,
-                "headers": {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "*"
-                },
-                "body": JSON.stringify(response.error)
-            });
-        } else if (response.validated) {
-           readQR(event.queryStringParameters, callback);
-        } else {
-            callback(null, {
-                "isBase64Encoded": true,
-                "statusCode": 403,
-                "headers": {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "*"
-                },
-                "body": "User validation failed."
-            });
-        }
-    });
-
-}
-   
-   function readQR(event, callback) { 
-       dynamoDBService.getQR(event.qr).then(function (data) {
+    console.log('data 23e23', event);
+   dynamoDBService.getQR(event.queryStringParameters.qr).then(function (data) {
        console.log('promoData 2e3', data);
        let promo = {
         promo: data.Item.promoId,
@@ -46,10 +14,11 @@ exports.handler = function (event, context, callback) {
         dynamoDBService.getPromoWithPromoId(promo).then(function (promoData) {
 
             console.log("data23221454", promoData);
-            console.log('promoData', promoData);
+            console.log('promoData', promoData !== null);
             if(promoData !== null){
+                 console.log('promoData 46', promoData.Items);
             if(data.Item.type === promoData.Items[0].OfferType){
-               
+              
               let promo = {
                     data: promoData.Items[0],
                     user: data.Item.user,
