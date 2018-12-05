862702bd7630702a3f940983fa4b72720d562cc8
TestAndunR <andunranmal@gmail.com>
2018-12-05 15:43:49 +0530

    update get promotion lambda

--- HotSpaces-Vendor/getPromotions.js
+++ HotSpaces-Vendor/getPromotions.js
@@ -4,57 +4,59 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 let moment = require('moment');
 let axios = require('axios');
 
-exports.handler = function(event, context, callback) {
-   console.log(event);
-    let date = moment(new Date(Number(event.queryStringParameters.date))).format('YYYY-MM-DD');
+exports.handler = function (event, context, callback) {
+    console.log(event);
+    let date = moment.unix(Number(event.queryStringParameters.date)).format('YYYY-MM-DD');
     console.log(date);
-    let promos = {};
+
     dynamoDBService.retrievePromos(date).then(function (data) {
         // axios.get()
         // console.log(data);
-        
+
         for (let i = 0; i < data.Items.length; i++) {
-            let promoId = data.Items[i].PromoId
-            promos[promoId] = {
-                    title: data.Items[i].Title,
-                    description: data.Items[i].Description,
-                    startDate: data.Items[i].StartDate,
-                    endDate: data.Items[i].EndDate,
-                    startTime: data.Items[i].StartTime,
-                    endTime: data.Items[i].EndTime,
-                    vendorId: data.Items[i].VendorId,
-                    businessType: data.Items[i].BusinessType,
-                    discount: data.Items[i].Discount,
-                    imgs: data.Items[i].ImgUrls,
-                    offerType: data.Items[i].OfferType,
-                    terms: data.Items[i].Terms,
+            let promos = {};
+            let promoId = data.Items[i].promoId
+            // console.log("Third", promos);
+            let vendorId = data.Items[i].vendorId;
+            dynamoDBService.getVendor("33cc6431-a133-447d-82f8-40b1529ee2bd").then(function (vendorData) {
+                //your logic goes here
+                console.log("First", vendorData.Items[0].name);
+                promos[promoId] = {
+                    title: data.Items[i].title,
+                    description: data.Items[i].description,
+                    startDate: data.Items[i].startDate,
+                    endDate: data.Items[i].endDate,
+                    startTime: data.Items[i].startTime,
+                    endTime: data.Items[i].endTime,
+                    vendorId: data.Items[i].vendorId,
+                    businessType: data.Items[i].businessType,
+                    discount: data.Items[i].discount,
+                    imgs: data.Items[i].imgUrls,
+                    offerType: data.Items[i].offerType,
+                    terms: data.Items[i].termsNConditions,
                     selectedDays: data.Items[i].selectedDays,
-                    unitPrice: data.Items[i].UnitPrice 
+                    unitPrice: data.Items[i].unitPrice,
+                    latNLong: data.Items[i].latNLong,
+                    address: data.Items[i].address,
+                    vendorName: vendorData.Items[0].name
             }
-            console.log("Third", promos.promoId);
-            let vendorId = data.Items[i].VendorId;
-            // dynamoDBService.getVendor("432aebe0-dba8-4aba-8881-30d54e70b84d").then(function (vendorData) {
-            //     //your logic goes here
-            //     console.log("First", vendorData.Item.name);
-            //     console.log("Second", promoId);
-            //     // console.log("Third", promos.promoId);
-            //     // promos.data.Items[i].PromoId["vendorName"] = vendorData.Item.name;
-            // }).catch(function (err) {
-            //     //handle error
-            //     console.log(err);
-            // });
-            
+
+                callback(null, {
+                    "isBase64Encoded": true,
+                    "statusCode": 200,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "*"
+                    },
+                    "body": JSON.stringify(promos)
+                });
+            }).catch(function (err) {
+                //handle error
+                console.log(err);
+            });
         }
         // console.log(promos);
-        callback(null, {
-            "isBase64Encoded": true,
-            "statusCode": 200,
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "*"
-            },
-            "body": JSON.stringify(data.Items)
-        });
+
     }).catch(function (err) {
         callback(null, {
             "isBase64Encoded": true,
