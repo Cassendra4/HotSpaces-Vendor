7f542794cf057654b96be149362f03d4a71b4daf
TestAndunR <andunranmal@gmail.com>
2018-12-11 17:18:55 +0530

    add new s3 as a resource

--- HotSpaces-Vendor/s3FileUpload.js
+++ HotSpaces-Vendor/s3FileUpload.js
@@ -1,5 +1,19 @@
-exports.handler = function(event, context, callback) {
-    console.log("opbject created");
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = function (event, context, callback) {
+    console.log("opbject created", event.Records[0].s3);
+    s3.listObjects({
+        'Bucket': 'aws-amplify-hotspaces',
+        'MaxKeys': 10,
+        'Prefix': ''
+    }).promise()
+        .then(data => {
+            console.log(data);           // successful response
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
     console.log(event);
-    callback(null, {"message": "Successfully executed"});
+    callback(null, { "message": "Successfully executed" });
 }
