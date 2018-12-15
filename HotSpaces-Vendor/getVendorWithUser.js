551e8b3195764bb9942f897a730d7e1eccd29098
TestAndunR <andunranmal@gmail.com>
2018-12-15 22:12:30 +0530

    Add the callback

--- HotSpaces-Vendor/getVendorWithUser.js
+++ HotSpaces-Vendor/getVendorWithUser.js
@@ -4,17 +4,35 @@ let dynamoDBService = require('./dynamoDBService');
 
 
 exports.handler = function (event, context, callback) {
-
+    console.log(event);
     let userId = event.queryStringParameters.userId;
     dynamoDBService.getUser(userId)
 
         .then(function (data) {
             //your logic goes here
             console.log(data);
+            callback(null, {
+                "isBase64Encoded": true,
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "*"
+                },
+                "body": JSON.stringify(data)
+            })
         })
         .catch(function (err) {
             //handle error
             console.log(err);
+            callback(null, {
+                "isBase64Encoded": true,
+                "statusCode": 502,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "*"
+                },
+                "body": err.message
+            })
         });
 
 
