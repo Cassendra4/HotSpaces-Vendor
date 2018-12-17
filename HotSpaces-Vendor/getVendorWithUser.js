cc34e10101d42b04009ab76e18f318a6b6986ce4
TestAndunR <andunranmal@gmail.com>
2018-12-17 11:31:55 +0530

    fix

--- HotSpaces-Vendor/getVendorWithUser.js
+++ HotSpaces-Vendor/getVendorWithUser.js
@@ -12,7 +12,29 @@ exports.handler = function (event, context, callback) {
             userData["userRole"] = data.Items[0].role;
             console.log(data.Items[0].vendor_id);
             let vendorId = data.Items[0].vendor_id;
+            ddb.get({
+                TableName: 'HS_Vendor',
+                Key: { 'vendor_id': vendorId }
+            }).promise()
+                .then(function (data) {
+                    console.log("vendor", data);
+                    userData["vendorName"] = data.Item.name;
+                    callback(null, {
+                        "isBase64Encoded": true,
+                        "statusCode": 200,
+                        "headers": {
+                            "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Methods": "*"
+                        },
+                        "body": JSON.stringify(userData)
                     })
+                    // userData[""]
+                    //your logic goes here
+                }).catch(function (err) {
+                    //handle error
+                    console.log(err);
+                });
+        })
         .catch(function (err) {
             //handle error
             console.log(err);
