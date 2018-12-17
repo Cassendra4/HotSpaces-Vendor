da6502274bd2f984d384a11ea86bedc8d44b3267
TestAndunR <andunranmal@gmail.com>
2018-12-17 10:55:43 +0530

    Add new db method

--- HotSpaces-Vendor/getVendorWithUser.js
+++ HotSpaces-Vendor/getVendorWithUser.js
@@ -1,24 +1,39 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 let dynamoDBService = require('./dynamoDBService');
 
 
 exports.handler = function (event, context, callback) {
-    console.log("aaaaa",event);
+    console.log("aaaaa", event);
+    let userData = {};
     let userId = event.queryStringParameters.userId;
     dynamoDBService.getUser(userId)
-
         .then(function (data) {
-            //your logic goes here
-            console.log(data);
-            callback(null, {
-                "isBase64Encoded": true,
-                "statusCode": 200,
-                "headers": {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "*"
-                },
-                "body": JSON.stringify(data)
-            })
+            userData["userRole"] = data.Items[0].role;
+            console.log(data.Items[0].vendor_id);
+            let vendorId = data.Items[0].vendor_id;
+            ddb.get({
+                TableName: 'HS_Vendor',
+                Key: { 'vendor_id': vendorId }
+            }).promise()
+                .then(function (data) {
+                    console.log("vendor", data);
+                    userData["vendorName"] = data.Item.name;
+                    callback(null, {
+                        "isBase64Encoded": true,
+                        "statusCode": 200,
+                        "headers": {
+                            "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Methods": "*"
+                        },
+                        "body": JSON.stringify(userData)
+                    })
+                    // userData[""]
+                    //your logic goes here
+                }).catch(function (err) {
+                    //handle error
+                    console.log(err);
+                });
         })
         .catch(function (err) {
             //handle error
@@ -33,5 +48,5 @@ exports.handler = function (event, context, callback) {
                 "body": err.message
             })
         });
-        
+
 }
