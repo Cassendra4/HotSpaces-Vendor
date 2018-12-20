34089bf532352a656389070205bbba467e47adca
TestAndunR <andunranmal@gmail.com>
2018-12-20 11:51:31 +0530

    add ddb and get no of redeems

--- HotSpaces-Vendor/getPromotion.js
+++ HotSpaces-Vendor/getPromotion.js
@@ -1,29 +1,52 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 let dynamoDBService = require('./dynamoDBService');
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     let vendorID = event.queryStringParameters.vendorId;
     console.log(vendorID);
     dynamoDBService.getPromo(vendorID).then(function (data) {
+        let promoData = data.Items;
+
+
+        Promise.all(promoData.map(promo => new Promise(((resolve, reject) =>
+            ddb.scan({
+                TableName: 'HS_Redeem',
+                ExpressionAttributeValues: {
+                    ':promoId': promo
+                },
+                FilterExpression: 'promoId = :promoId'
+            }).promise().then(function (data) {
+                console.log(data);
+                resolve(promoData["redeems"] = data.Items[0].noOfRedeems)
+                //your logic goes here
+            }).catch(function (err) {
+                //handle error
+                resolve();
+            })
+        )))).then(
+            console.log("Promoss###",promoData)
+        )
+
         console.log(data);
-            callback(null, {
-                "isBase64Encoded": true,
-                "statusCode": 200,
-                "headers": {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "*"
-                },
-                "body": JSON.stringify(data.Items)
-            });
-        }).catch(function (err) {
-            callback(null, {
-                "isBase64Encoded": true,
-                "statusCode": 502,
-                "headers": {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "*"
-                },
-                "body": err.message
-            });
+        callback(null, {
+            "isBase64Encoded": true,
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "*"
+            },
+            "body": JSON.stringify(data.Items)
         });
+    }).catch(function (err) {
+        callback(null, {
+            "isBase64Encoded": true,
+            "statusCode": 502,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "*"
+            },
+            "body": err.message
+        });
+    });
 }
