4a4c16d81fc874b2fe9cc8909cfe621a1cce7ed3
TestAndunR <andunranmal@gmail.com>
2019-01-07 12:37:38 +0530

    fix to new lambda

--- HotSpaces-Vendor/addUser2.js
+++ HotSpaces-Vendor/addUser2.js
@@ -6,31 +6,23 @@ exports.handler = function (event, context, callback) {
     let phone = event.phone;
     let role = event.role;
     cognito_idp.adminCreateUser({
-        UserPoolId: process.env.UserPoolId_cognitoHotspacesVendor,
+        UserPoolId: process.env.UserPoolId_cognitoHotspaceVendor,
         Username: `${email}`,
         DesiredDeliveryMediums: ["EMAIL", "SMS"],
         ForceAliasCreation: false,
         TemporaryPassword: "12345678",
-        UserAttributes: [{
-            Name: "email",
-            Value: `${email}`
-        }, {
-            Name: "phone_number",
-            Value: `${phone}`
-        }, {
-            Name: "custom:Role",
-            Value: `${role}`
-        }],
+        UserAttributes: [{ Name: "custom:role", Value: `${role}` }],
         ValidationData: []
     }, function (error, data) {
         if (error) {
-            console.log(error);
+            console.log(error)
             // implement error handling logic here
             throw error;
         }
-        console.log(data)
         // your logic goes within this block
+        console.log(data);
     });
+
 
     callback(null, { "message": "Successfully executed" });
 }
